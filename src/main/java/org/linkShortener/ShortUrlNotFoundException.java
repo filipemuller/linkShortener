b38129dfd05cbf7a32f8c9c1b0772efc3a36ebcc
fmuller <filipemullersilva@gmail.com>
2023-04-05 09:49:35 -0400

    Update package name and minor changes

--- src/main/java/org/linkShortener/ShortUrlNotFoundException.java
+++ src/main/java/org/linkShortener/ShortUrlNotFoundException.java
@@ -4,7 +4,5 @@ package org.linkShortener;
 
 public class ShortUrlNotFoundException extends RuntimeException{
 
-    System.out.print("Erro!");
-
     //Essa classe aina está em desenvolvimento e irá lidar com os erros
 }
